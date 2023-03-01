d44b1573fde9a109fd4765e0a9fc8d4f8f6780cd
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-03-01 17:04:36 +0000

    Create maven component and using it

---
+++ Java/maven-exemplo/src/main/java/com/example/Parser.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Parser {
+    
+    public void message() {
+        System.out.println("Hello from component!");
+    }
+}
---
+++ Java/maven-usarcomponente/src/main/java/com/teste/App.java
@@ -0,0 +1,17 @@
+package com.teste;
+
+import com.example.Parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Parser parser = new Parser();
+
+        parser.message();
+    }
+}
